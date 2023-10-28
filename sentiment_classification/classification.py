c5d2bac1db6cc350fe4890bbd8cebd3a276f29a1
Kushal Kodnad <kkodnad@berkeley.edu>
2023-10-28 13:55:16 -0700

    added sentiment classification function

---
+++ sentiment_classification/classification.py
@@ -0,0 +1,31 @@
+import asyncio # must import asyncio to run the actual function
+from hume import HumeStreamClient
+from hume.models.config import LanguageConfig
+
+# import sys
+# {sys.executable} -m pip install "hume[stream]"
+# {sys.executable} -m pip install hume
+# {sys.executable} -m pip install websockets
+
+
+async def main(text):
+    client = HumeStreamClient("sa4h1KJloRvXssnhlFfGTldG7QiaYyVrJgWrUNR3y0KWzGA8")
+    config = LanguageConfig()
+    async with client.connect([config]) as socket:
+        result = await socket.send_text(text)
+        emotions = result["language"]["predictions"][0]["emotions"]
+    scores = [emotions[i]["score"] for i in range(53)]
+    emotions = ['Admiration', 'Adoration', 'Aesthetic Appreciation', 'Amusement', 'Anger', 'Annoyance', 'Anxiety', 'Awe', 
+                'Awkwardness', 'Boredom', 'Calmness', 'Concentration', 'Confusion', 'Contemplation', 'Contempt', 'Contentment', 
+                'Craving', 'Determination', 'Disappointment', 'Disapproval', 'Disgust', 'Distress', 'Doubt', 'Ecstasy', 
+                'Embarrassment', 'Empathic Pain', 'Enthusiasm', 'Entrancement', 'Envy', 'Excitement', 'Fear', 'Gratitude', 
+                'Guilt', 'Horror', 'Interest', 'Joy', 'Love', 'Nostalgia', 'Pain', 'Pride', 'Realization', 'Relief', 'Romance', 
+                'Sadness', 'Sarcasm', 'Satisfaction', 'Desire', 'Shame', 'Surprise (negative)', 'Surprise (positive)', 
+                'Sympathy', 'Tiredness', 'Triumph']
+    index, maximum = -1, 0
+    for i in range(53):
+        if scores[i] > maximum:
+            maximum, index = scores[i], i
+    return emotions[index]
+
+# print(asyncio.run(main(text="")))
