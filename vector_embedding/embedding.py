14b6d5584e004bc31dbac7a88b633bcf22b2404f
Rushil Desai <rushil.desai.04@gmail.com>
2023-10-28 14:33:19 -0700

    Embeddings

--- vector_embedding/embedding.py
+++ vector_embedding/embedding.py
@@ -1,13 +1,12 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-model = AutoModel.from_pretrained("bert-base-uncased")
+def generate_primary_embedding(sentence):
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    model = AutoModel.from_pretrained("bert-base-uncased")
 
-sentence = "This is a sample sentence."
-
-# Tokenize and obtain the sentence embedding
-inputs = tokenizer(sentence, return_tensors="pt", padding=True, truncation=True)
-outputs = model(**inputs)
-sentence_vector = torch.mean(outputs.last_hidden_state, dim=1)
-print(sentence_vector)
+    # Tokenize and obtain the sentence embedding
+    inputs = tokenizer(sentence, return_tensors="pt", padding=True, truncation=True)
+    outputs = model(**inputs)
+    sentence_vector = torch.mean(outputs.last_hidden_state, dim=1)
+    return sentence_vector
