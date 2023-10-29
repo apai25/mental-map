e59a879ac596480ceb15ee30bcc5f94bfa2c8b90
Kushal Kodnad <kkodnad@berkeley.edu>
2023-10-28 18:07:22 -0700

    added interactive chatbot function

---
+++ interactive_chatbot/chatbot.py
@@ -0,0 +1,46 @@
+import subprocess
+python_executable = '/usr/local/bin/python3'
+package_names = ['openai']
+for package_name in package_names:
+    subprocess.check_call([python_executable, '-m', 'pip', 'install', package_name])
+
+import openai
+import random
+
+def get_gpt3_response(prompt):
+    prompt = "You are a friend talking to another person. Use slang language in your response, and text like a college student. Respond to this message: " + prompt
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # You can use other models like text-davinci-003
+        prompt=prompt,
+        max_tokens=100,  # Adjust as needed
+        n=1,
+        stop=None,
+        temperature=0.6  # Adjust for desired randomness
+    )
+    return response.choices[0].text.strip()
+
+def run_chatbot():
+    questions = ["Rose?", "Bud?", "Thorn?"]
+
+    api_key = "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"
+    openai.api_key = api_key
+
+    start = True
+    while True:
+        if start:
+            print("To end your conversation, please enter any of the following: 'Quit', 'Exit', 'Q', or 'Done'")
+            user_input = input(random.choice(questions) + " You: ")
+            start = False
+        else:
+            user_input = input("You: ")
+
+        if user_input.lower() in ['quit', 'exit', 'q', 'done']:
+            print("Exiting...")
+            break
+
+        gpt3_response = get_gpt3_response(user_input)
+
+        print(f"Chat: {gpt3_response}")
+
+
+run_chatbot()
