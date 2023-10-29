99b7e6451691953e0ee54635cee0802059a37147
Anirudh Pai <anirudhnpai@gmail.com>
2023-10-28 17:45:57 -0700

    backend tested, services work (not very robust)

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from utils.sentiment_classification import get_sentiment
 from flask_cors import CORS
 import os
 import psycopg
-import bcrypt 
+from datetime import datetime
 
 entry_id = 1
 user_id = 1
@@ -18,50 +18,61 @@ def login():
 
     with conn.cursor() as cursor:
         cursor.execute(
-            "SELECT user_id, email, password_hash FROM users WHERE email = %s",
-            (data['email'])
+            "SELECT (user_id, email, user_password) FROM user_information WHERE email = %s",
+            (data['email'],)
         )
         user_information = cursor.fetchone()
 
     if user_information is None:
         return 'Email or password is incorrect', 401
-    
-    if not bcrypt.checkpw(data['password'], user_information['password_hash']):
+
+    if data['password'] != user_information[0][2]:
         return 'Email or password is incorrect', 401
+
     return 'Login successful', 200
 
 @app.route('/register', methods=['POST'])
 def register():
     global user_id
     data = request.json
-    password_hash = bcrypt.hashpw(data['password'], bcrypt.gensalt())
+
+
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT user_id FROM user_information WHERE email = %s", (data['email'],))
+        existing_user = cursor.fetchone()
+
+    if existing_user:
+        return "User already registered.", 400
 
     with conn.cursor() as cursor:
         cursor.execute(
-            "INSERT INTO user_information (user_id, email, password_hash) VALUES (%s, %s, %s)",
-            (user_id, data['email'], password_hash)
+            "INSERT INTO user_information (user_id, email, user_password) VALUES (%s, %s, %s)",
+            (user_id, data['email'], data['password'])
         )
     user_id = user_id + 1
     conn.commit()
-    return 200
+    return 'OK', 200
 
 @app.route('/store-entry', methods=['POST'])
-def store_entry():
+async def store_entry():
     global entry_id
     data = request.json
-    current_timestamp = "CURRENT_TIMESTAMP"
-    sentiment = get_sentiment(data['entry_text'])
+    sentiment = await get_sentiment(data['entry_text'])
+
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%m/%d/%Y")
 
     with conn.cursor() as cursor:
+
         cursor.execute(
-            "INSERT INTO diary_entries (entry_id, user_id, entry_time, entry_text, sentiment) VALUES (%s, %s, " + current_timestamp + ", %s, %s)",
-            (entry_id, data['user_id'], data['entry_text'], sentiment)
+            "INSERT INTO diary_entries (entry_id, user_id, entry_date, entry_text, sentiment) VALUES (%s, %s, %s, %s, %s)",
+            (entry_id, data['user_id'], formatted_date, data['entry_text'], sentiment)
         )
 
     entry_id = entry_id + 1
 
     conn.commit()
-    return 200
+    return 'OK', 200
 
 @app.route('/get-entries', methods=['POST'])
 def get_entries():
@@ -70,17 +81,17 @@ def get_entries():
 
     with conn.cursor() as cursor:
         cursor.execute(
-            "SELECT entry_id, user_id, entry_time, sentiment, entry_text, sentiment FROM diary_entries WHERE user_id = %s",
+            "SELECT (entry_id, user_id, entry_date, sentiment, entry_text) FROM diary_entries WHERE user_id = %s",
             (user_id,)
         )
         entries = cursor.fetchall()
 
-    keys = ['entry_id', 'user_id', 'entry_time', 'sentiment', 'entry_text', 'sentiment']
-    entry_information = [{keys[i]: entry[i] for i in range(len(keys))} for entry in entries]
+    keys = ['entry_id', 'user_id', 'entry_date', 'sentiment', 'entry_text']
+    entry_information = [{keys[i]: entry[0][i] for i in range(len(keys))} for entry in entries]
 
-    return jsonify(entry_information)
+    return jsonify(entry_information), 200
 
 if __name__ == '__main__':
     HOST = 'localhost'
     PORT = 3000
-    app.run(host=HOST, port=PORT)
+    app.run(host=HOST, port=PORT, debug=True)
